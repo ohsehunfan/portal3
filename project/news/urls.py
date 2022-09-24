2500598a776b738ce7a8e6c89cb4ca754e2b1d55
Lena <toujoursbelleee@icloud.com>
2022-09-24 13:51:05 +0300

    Initial Commit

---
+++ project/news/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import PostList, PostDetail, create_post, PostUpdate, PostDelete
+
+urlpatterns = [
+   path('', PostList.as_view(), name='post_list'),
+   path('<int:pk>', PostDetail.as_view(), name='post_detail'),
+   path('create/', create_post, name='post_create'),
+   path('<int:pk>/update/', PostUpdate.as_view(), name='post_update'),
+   path('<int:pk>/delete/', PostDelete.as_view(), name='post_delete'),
+]
